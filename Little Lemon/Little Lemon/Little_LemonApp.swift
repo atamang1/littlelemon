4c40712dc0858e9d8c70619607bc05a8885d9c35
Ashok <ashoktamang1108@gmail.com>
2026-03-03 10:48:09 -0500

    test: ios project

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Anisha Gurung on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
