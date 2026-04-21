e85bdf005f2a13e54b62f66673b88197826bd082
Ashok <ashoktamang1108@gmail.com>
2026-04-21 15:57:12 -0400

    added Home.swift and Menu.swift files

---
+++ Little Lemon/Little Lemon/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Little Lemon
+//
+//  Created by Anisha Gurung on 4/21/26.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    Home()
+}
---
+++ Little Lemon/Little Lemon/Menu.swift
@@ -0,0 +1,18 @@
+//
+//  Menu.swift
+//  Little Lemon
+//
+//  Created by Anisha Gurung on 4/21/26.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu()
+}
