db4a7f63e42cb7ad7cb5a6e51f4a9f31aa8b6106
Ashok <ashoktamang1108@gmail.com>
2026-04-30 08:48:01 -0400

    updated: menu ui that align with the wireframe. Added customs color and fonts

--- Little Lemon/Little Lemon/Menu.swift
+++ Little Lemon/Little Lemon/Menu.swift
@@ -13,6 +13,9 @@ struct Menu: View {
     
     //to store search input
    @State var searchText = ""
+    @State var activeCategory:String? = nil // keep track of category button press
+
+    var categories = ["Starters", "Mains", "Desserts", "Sides"]
     
     // Querying the server
     func getMenuData () {
@@ -58,14 +61,24 @@ struct Menu: View {
     }
     
     //this function returns a predicte to filter the FetchedObjects result
-    func buildPredicate () -> NSPredicate {
+    func buildPredicate (category: String) -> NSPredicate {
         //if search text is empty then show all list
-        if searchText.isEmpty {
+        if searchText.isEmpty && category.isEmpty {
             return NSPredicate(value: true)
         }
         
+        // 2. If searching but no category selected
+           if !searchText.isEmpty && category.isEmpty {
+               return NSPredicate(format: "title CONTAINS[cd] %@", searchText)
+           }
+        
+        // 3. If category is selected but no search text
+            if searchText.isEmpty && !category.isEmpty {
+                return NSPredicate(format: "category == %@", category.lowercased())
+            }
+           
         // return only list that matches the search text
-        return NSPredicate(format: "title CONTAINS[cd] %@", searchText)
+        return NSPredicate(format: "title CONTAINS[cd] %@ AND category == %@", searchText, category.lowercased())
     }
     
     
@@ -79,39 +92,123 @@ struct Menu: View {
         )]
     }
     
+    /*
+    // filter by categories
+    func categorySortPredicate ( _ category: String) -> NSPredicate {
+        // if argumentis empty then show all list
+        if category.isEmpty {
+            return NSPredicate(value: true)
+        }
+        
+        // return only list that matches the exact category
+        return NSPredicate(format: "category == %@", category.lowercased())
+    }
+     */
+    
     
     var body: some View {
-        VStack {
+        VStack(){
             
             // Head Logo and Profile
             HStack{
+                Spacer()
                 Image("Logo")
                     .resizable()
-                    .frame(width: 200, height: 50)
+                    .frame(width: 200, height: 30)
                     .padding(.horizontal,30)
+                Spacer()
                 Image("Profile-image-placeholder")
                     .resizable()
-                    .frame(width: 70, height: 70)
+                    .frame(width: 30, height: 30)
+                    
             }
+            .padding(10)
+            .frame(height: 30)
             // About Restaurant
-            VStack(alignment: .leading){
-                Text("Little Lemon")
-                    .font(.system(size: 64))
+            VStack(alignment: .leading, spacing: 0){
+                
+                Text("Little lemon")
+                    .font(.custom("MarkaziText-Medium", size: 64))
+                    .foregroundColor(.yellow)
+                    .padding(0)
                 Text("Chicago")
-                    .font(.system(size: 40))
-                Text("We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
-                    .font(.system(size: 18))
-              
-                TextField("Search menu", text: $searchText)
+                    .font(.custom("MarkaziText-Regular", size: 40))
+                    .padding(.top, -10)
+                
+                    //.font(.system(size: 40))
+                
+                // About description and Hero image
+                HStack{
+                    Text("We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
+                        .font(.custom("Karla-Medium", size: 18))
+                        .fixedSize(horizontal: false, vertical: true) // Forces it to expand vertically to fit the text
+                        .padding(.top, -20)
+                    Spacer()
+                    
+                    Image("Hero image")
+                        .resizable()
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .frame(width: 168, height: 153)
+
+                }
+                .padding(0)
+                
+                // Search menu text field
+                HStack{
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .padding(10)
+                    
+                    TextField("Search menu", text: $searchText)
+                        .foregroundColor(.black)
+                        .frame(height: 40)
+                }
+                .background(Color.white)
+                .cornerRadius(8)
+                .padding(.bottom, 10)
+                .padding(.top, 10)
+                
             }
-            .padding(20)
-            .frame(maxWidth: .infinity)
-            .background(Color.green)
+            .padding(10)
+            .background(Color.primaryGreen)
+            .frame(maxHeight: .infinity)
             
+            //Tapable Categories
+            VStack(alignment: .leading) {
+                Text("ORDER FOR DELIVERY! ")
+                    .font(.custom("Karla-ExtraBold", size: 20))
+               
+                //Categories
+                HStack(spacing: 20){
+                    /*
+                    Toggle("Starter", isOn: $isHighlighted)
+                        .toggleStyle(.button) // Makes it look and behave like a persistent button
+                        .tint(.blue) // Changes the highlight color
+                        .font(.custom("Karla-Medium", size: 16))
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.blue, lineWidth: 2)
+                                .frame(width: 80, height: 30)
+                        }
+                     */
+                    
+                    ForEach(categories, id: \.self) { category in
+                        Button(category) {
+                            // If this button is already the active one, turn it off
+                            activeCategory = (activeCategory == category) ? nil: category
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(activeCategory == category ? .blue : .gray)
+                        .disabled(activeCategory != nil && activeCategory != category)
+                       
+                    }
+                        
+                }
+            }
            
            //this will fetch all the Dishes from the core data
             // and make them available to use in the closure.
-            FetchedObjects(predicate: buildPredicate(), sortDescriptors: buildSortDescriptors()) { (dishes: [Dish]) in
+            FetchedObjects(predicate: buildPredicate(category: activeCategory ?? ""), sortDescriptors: buildSortDescriptors()) { (dishes: [Dish]) in
                 List {
                     ForEach(dishes) { dish in
                         NavigationLink(destination: Details(dish: dish)){
---
+++ Little Lemon/Little Lemon/styles/BrandColors.swift
@@ -0,0 +1,17 @@
+//
+//  BrandColor.swift
+//  Little Lemon
+//
+//  Created by Ashok Tamang on 4/28/26.
+//
+
+import SwiftUI
+public extension Color {
+    static let primaryGreen = Color(red: 73/255, green: 94/255, blue: 87/255)
+    static let primaryYellow = Color(red: 244/255, green: 206/255, blue: 20/255)
+    static let secondaryOrange = Color(red: 238/255, green: 153/255, blue: 114/255)
+    static let secondaryPaleOrange = Color(red:251/255, green: 218/255, blue: 187/255)
+    static let highlightWhite = Color(red: 247/255, green: 239/255, blue: 238/255)
+    static let highlightBlack = Color(red: 51/255, green: 51/255, blue: 51)
+    
+}
